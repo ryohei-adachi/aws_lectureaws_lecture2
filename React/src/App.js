e152a8531521ae92b13f195986fab3f1fa4eff0d
ryohei-adachi <75190594+ryohei-adachi@users.noreply.github.com>
2024-05-19 10:20:19 +0900

    Add files via upload

--- React/src/App.js
+++ React/src/App.js
@@ -11,83 +11,100 @@ const App = () =>{
 
   useEffect(()=>{
 
-    axios
-    .get("https://f8ihn5u237.execute-api.ap-northeast-1.amazonaws.com/production/task-list")
-    .then((response)=>{
-        //タスク情報を取得
+      axios
+      .get("https://f8ihn5u237.execute-api.ap-northeast-1.amazonaws.com/production/task-list")
+      .then((response)=>{
+            //タスク情報を取得
         
-        const responseTasks = response.data.tasks;
-        let initTasks=[];
+            const responseTasks = response.data.tasks;
+            let initTasks=[];
         
-        responseTasks.map((responseTask) => {
+            responseTasks.map((responseTask) => {
 
-          const initTask = {
-            id: responseTask.id,
-            taskName: responseTask.taskName
-          };
-          initTasks = initTasks.concat(initTask);
-          console.log(initTasks);
+            const initTask = {
+                id: responseTask.id,
+                taskName: responseTask.taskName
+            };
+            initTasks = initTasks.concat(initTask);
+            console.log(initTasks);
 
-        })
-        setTasks(initTasks);
-
-        let initId = responseTasks.length;
-        setId(initId + 1);
+            })
+          setTasks(initTasks);
+          let initId = responseTasks.length;
+          setId(initId + 1);
 
         
-    })
-    .catch((error)=>{
-        //エラー
-        console.log(error);
-    })
+      })
+      .catch((error)=>{
+          //エラー
+          console.log(error);
+      })
 
   },[]);
   
 
   //登録ボタンを押下時のイベント
   const onSave = (taskName) =>{
-    const newTask = {
-      id: id,
-      taskName: taskName
-    };
+      const newTask = {
+          id: id,
+          taskName: taskName
+      };
 
-    //POST通信でDynamoDBにタスク情報を登録する
-    axios.post("https://f8ihn5u237.execute-api.ap-northeast-1.amazonaws.com/production/task",newTask)
-    .then((response)=>{
-      console.log(response);
+      //登録用のAPIを呼び出し
+      axios.post("https://f8ihn5u237.execute-api.ap-northeast-1.amazonaws.com/production/task",newTask)
+      .then((response)=>{
+          console.log(response);
 
-      //フロント側でも追加したタスクを表示する
-      setId(id + 1);
-      const newTasks = tasks.concat(newTask)
-      setTasks(newTasks);
-    })
-    .catch((error)=>{
-      console.log(error);
-    })
+          //フロント側でも追加したタスクを表示する
+          setId(id + 1);
+          const newTasks = tasks.concat(newTask)
+          setTasks(newTasks);
+      })
+      .catch((error)=>{
+          console.log(error);
+      })
 
   };
 
   const onDelete = (id) => {
-    axios
-    .delete("https://f8ihn5u237.execute-api.ap-northeast-1.amazonaws.com/production/task/" + id)
-    .then((response)=>{
+      //削除用のAPIを呼び出し
+      axios
+      .delete("https://f8ihn5u237.execute-api.ap-northeast-1.amazonaws.com/production/task/" + id)
+      .then((response)=>{
       
-      //フロント側では、削除したID以外のタスクをリストとして再作成して、再セットする。
-      const filteredTasks = tasks.filter((task) => task.id !== id);
-      setTasks(filteredTasks);
+          //フロント側では、削除したID以外のタスクをリストとして再作成して、再セットする。
+          const filteredTasks = tasks.filter((task) => task.id !== id);
+          setTasks(filteredTasks);
 
-    })
-    .catch((error)=>{
-      console.log(error);
-    })
-
+      })
+      .catch((error)=>{
+          console.log(error);
+      })
   };
 
   const onUpdate = (id, taskName) => {
-    const newTasks = tasks.map((task)=>
-      task.id === id ? { id:id, taskName:taskName } : task
-    );
-    setTasks(newTasks);
+  
+      const updateTask = {
+        id: id,
+        taskName: taskName
+      };
+
+      //更新用のAPIを呼び出し
+      axios
+      .put("https://f8ihn5u237.execute-api.ap-northeast-1.amazonaws.com/production/task",updateTask)
+      .then((response)=>{
+
+          //更新対象のタスクを更新して、リストを作り直す
+          const newTasks = tasks.map((task)=>
+              task.id === id ? updateTask : task
+          );
+          setTasks(newTasks);
+      })
+      .catch((error)=>{
+          console.log(error);
+      })
+
+      
   }
 
   return (
